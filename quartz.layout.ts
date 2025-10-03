fa0ec9ff525c6349cc190e39921bd7e9e379c7f0
duckyfuz <108561447+duckyfuz@users.noreply.github.com>
2025-10-03 22:07:43 +0800

    layout: update footer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/duckyfuz/quartz-notes/tree/kenf-main-dev",
+      Portfolio: "https://www.kenf.dev/",
+      LinkedIn: "https://www.linkedin.com/in/thisiskenf/",
     },
   }),
 }
